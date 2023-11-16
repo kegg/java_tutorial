539d7bb86b816a845972d4cae4b83bf12c606ab4
Kyle Eggleston <kyle@dreamsense.com>
2023-11-15 18:55:51 -0700

    reverted

--- app/src/main/java/javaTutorial/Main.java
+++ app/src/main/java/javaTutorial/Main.java
@@ -2,14 +2,9 @@ package javaTutorial;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.util.*;
 
-public class Main extends JFrame implements ActionListener {
-
-    private JComboBox box;
-
+public class Main extends JFrame {
     public static void main(String[] args) {
         Main main = new Main();
         main.setSize(500,500);
@@ -22,15 +17,7 @@ public class Main extends JFrame implements ActionListener {
         Vector<Item> items = new Vector<>();
         items.add(new Item(1, "John Smith"));
         items.add(new Item(2, "Susan Jones"));
-        box = new JComboBox(items);
-        JButton button = new JButton("Execute");
-        button.addActionListener(this);
+        JComboBox<Vector<Item>> box = new JComboBox(items);
         add(box, BorderLayout.NORTH);
-        add(button, BorderLayout.SOUTH);
-    }
-
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        System.out.println(((Item) box.getSelectedItem()).getId());
     }
 }
