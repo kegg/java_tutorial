08e99ff80995e34ff5f56d1385a6578dc47a54bf
Kyle Eggleston <kyle@dreamsense.com>
2023-11-15 19:11:10 -0700

    added some actions for the a button to display data

--- app/src/main/java/javaTutorial/Main.java
+++ app/src/main/java/javaTutorial/Main.java
@@ -2,10 +2,17 @@ package javaTutorial;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class Main extends JFrame {
+public class Main extends JFrame implements ActionListener {
+
+    private JComboBox<Item> box;
+    private JTextArea textarea;
+
     public static void main(String[] args) {
         Main main = new Main();
+        main.setTitle("Testing Combo Box");
         main.setSize(500,300);
         main.setLocationRelativeTo(null);
         main.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -16,7 +23,22 @@ public class Main extends JFrame {
         DefaultComboBoxModel<Item> cbo = new DefaultComboBoxModel<>();
         cbo.addElement(new Item(1, "John Smith"));
         cbo.addElement(new Item(2, "Susan Jones"));
-        JComboBox<Item> box = new JComboBox<>(cbo);
+        box = new JComboBox<>(cbo);
+        JButton button = new JButton("Get Data");
+        button.setActionCommand("press");
+        button.addActionListener(this);
+        textarea = new JTextArea();
         add(box, BorderLayout.NORTH);
+        add(textarea, BorderLayout.CENTER);
+        add(button, BorderLayout.SOUTH);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getActionCommand().equals("press")) {
+            long id = ((Item) box.getSelectedItem()).getId();
+            String name = ((Item) box.getSelectedItem()).toString();
+            textarea.setText("ID: " + id + "\n" + "Name: " + name);
+        }
     }
 }
