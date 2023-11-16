e82e11524bd4e56d0d273ee3a37ede3543fb834a
Kyle Eggleston <kyle@dreamsense.com>
2023-11-15 19:02:09 -0700

    added DefaultComboBoxModel

--- app/src/main/java/javaTutorial/Main.java
+++ app/src/main/java/javaTutorial/Main.java
@@ -2,22 +2,21 @@ package javaTutorial;
 
 import javax.swing.*;
 import java.awt.*;
-import java.util.*;
 
 public class Main extends JFrame {
     public static void main(String[] args) {
         Main main = new Main();
-        main.setSize(500,500);
+        main.setSize(500,300);
         main.setLocationRelativeTo(null);
         main.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         main.setVisible(true);
     }
 
     public Main() {
-        Vector<Item> items = new Vector<>();
-        items.add(new Item(1, "John Smith"));
-        items.add(new Item(2, "Susan Jones"));
-        JComboBox<Vector<Item>> box = new JComboBox(items);
+        DefaultComboBoxModel<Item> cbo = new DefaultComboBoxModel<>();
+        cbo.addElement(new Item(1, "John Smith"));
+        cbo.addElement(new Item(2, "Susan Jones"));
+        JComboBox<Item> box = new JComboBox<>(cbo);
         add(box, BorderLayout.NORTH);
     }
 }
